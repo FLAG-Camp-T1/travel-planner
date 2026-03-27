8b6a48b669b556e79855f532cd7b5e0b97ec3cf2
Christoph Chen <ugamegerm.chen@gmail.com>
2026-03-27 19:25:48 -0400

    feat(backend): add OpenAPI UI to generate API document

--- backend/src/main/java/com/travelplanner/backend/route/dto/RouteRequest.java
+++ backend/src/main/java/com/travelplanner/backend/route/dto/RouteRequest.java
@@ -1,13 +1,22 @@
 package com.travelplanner.backend.route.dto;
 
 import com.travelplanner.backend.route.enums.TravelMode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RouteRequest {
 
+    @Schema(
+            description = "Google Place ID of the starting location",
+            example = "ChIJVVVVVYx3j4ARP-3NGldc8qQ")
     private String originPlaceId;
+
+    @Schema(
+            description = "Google Place ID of the destination",
+            example = "ChIJJcSDXXx3j4ARRef7L0P3GpY")
     private String destinationPlaceId;
 
+    @Schema(description = "Method of transportation", example = "DRIVE")
     private TravelMode travelMode = TravelMode.DRIVE;
 }
